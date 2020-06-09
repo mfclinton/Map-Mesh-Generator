494df150b4f65f063f3ab23c2f61bb24585dddfb
mfclinton <mfclinton0@gmail.com>
2020-06-09 19:51:15 -0400

    basic pixel preprocessing

--- triagulation.py
+++ triagulation.py
@@ -1,41 +1,84 @@
 import numpy as np
 import matplotlib.pylab as plt
 import scipy.spatial as spatial
+import triangle as tr
 import cv2
 
-# Preparing File
-fileName = "red_circle.png"
+#Parameters
+fileName = "test_files/colors.png"
+
+#Image Processing
 img = cv2.imread(fileName)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-points = np.column_stack(np.where(gray == 0))
-points = np.insert(points, 2, 0, axis=1)
+unique_color_data = np.unique(img.reshape((1, -1, 3))[0,:],axis=0, return_counts=True)
+blacklist = np.array([[255,255,255], [0,0,0], [255,0,0]]) #NOTE, cv2 has pixels in bgr order
 
-# Select Points
-numPoints = 20
-chosenPoints = []
+for color in unique_color_data[0]:
+    #Skips Colors On Blacklist
+    if(np.any(np.all(blacklist == color, axis=-1))):
+        print(color)
+        continue
 
-for i in range(numPoints):
-    points = points[points[:,2].argsort()]
-    bestPoint = points[-1]
-    for p in points:
-        p[-1] += spatial.distance.euclidean(bestPoint, p)
-    bestPoint[-1] = -9999 #temp solution, should remove
+    coords = np.where(np.all(img == color, axis=-1))
+    
+    for i in range(len(coords[0])):
+        img[coords[0][i],coords[1][i]] = [0,0,0]
+
+cv2.imshow("image", img)
+cv2.waitKey(0)
 
 
-    chosenPoints.append([bestPoint[0], bestPoint[1]])
 
-chosenPoints = np.array(chosenPoints)
-print(chosenPoints)
 
-# print(np.insert(points, 2, 0, axis=1))
-# randomPoints = points[np.random.choice(points.shape[0], 100, replace = False)]
-# print(randomPoints)
 
-# plot triangle
-tri = spatial.Delaunay(chosenPoints)
-plt.triplot(chosenPoints[:,0], chosenPoints[:,1], tri.simplices)
-# plt.plot(points[:,0], points[:,1], 'o') # plot points
-plt.show()
+
+
+
+
+
+
+
+
+
+#--------------------------------
+
+# print(tr.get_data('face'))
+
+# # Preparing File
+# fileName = "test_files/europe.png"
+# img = cv2.imread(fileName)
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# points = np.column_stack(np.where(gray == 0))
+# dog = points
+# points = np.insert(points, 2, 0, axis=1)
+
+# # Select Points
+# numPoints = 200
+# chosenPoints = []
+
+# for i in range(numPoints):
+#     points = points[points[:,2].argsort()]
+#     bestPoint = points[-1]
+#     for p in points:
+#         dist = spatial.distance.euclidean(bestPoint, p)
+#         p[-1] += dist
+#     bestPoint[-1] = -9999 #temp solution, should remove
+
+
+#     chosenPoints.append([bestPoint[0], bestPoint[1]])
+
+# chosenPoints = np.array(chosenPoints)
+
+# # print(np.insert(points, 2, 0, axis=1))
+# # randomPoints = points[np.random.choice(points.shape[0], 100, replace = False)]
+# # print(randomPoints)
+
+# # plot triangle
+# A = dict(vertices = chosenPoints)
+# B = tr.triangulate(A)
+# tr.compare(plt, A, B)
+# # plt.triplot(chosenPoints[:,0], chosenPoints[:,1], tri.simplices)
+# # plt.plot(points[:,0], points[:,1], 'o') # plot points
+# plt.show()
 
 
 
