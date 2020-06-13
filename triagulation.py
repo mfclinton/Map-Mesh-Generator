62af9b56dca670a283b6d4ca3b0b6a21416528a3
mfclinton <mfclinton0@gmail.com>
2020-06-13 04:15:26 -0400

    basic area score metric

--- triagulation.py
+++ triagulation.py
@@ -4,24 +4,83 @@ import scipy.spatial as spatial
 import triangle as tr
 import cv2
 
+#Helper Functions
+
+#Calculates the accuracy of the PSLG
+def CalculateAccuracy(img, coords, points, line_segments):
+    # print(points)
+    # print(line_segments)
+    score = 0
+    for c in coords:
+        intersections = 0
+        for l in line_segments:
+            p1 = points[l[0]]
+            p2 = points[l[1]]
+
+            # quick terminate
+            if(not (p1[0] <= c[0] <= p2[0] or p2[0] <= c[0] <= p1[0])):
+                continue
+
+            #divide by zero error
+            if(p1[0] - p2[0] == 0):
+                if(p1[1] <= c[1] <= p2[1] or p2[1] <= c[1] <= p1[1]):
+                    intersections += 1
+                continue
+
+            x_intercept = (c[0] - p1[0])*(p1[1] - p2[1])/(p1[0] - p2[0]) + p1[1]
+
+            #intersects the line
+            if(c[1] <= x_intercept):
+                if(x_intercept != p1[1] and x_intercept != p2[1]):
+                    intersections += 1
+                #special corner interection case
+                elif((x_intercept == p1[1] and p1[0] > p2[0]) or (x_intercept == p2[1] and p2[0] > p1[0])):
+                    intersections += 1
+
+        if(intersections % 2 != 1):
+            continue
+
+        img[c[0],c[1]] = [0,0,0]
+        score += 1
+    return score
+
+
+
+def GetIntersections():
+    print("")
+
 #Parameters
 fileName = "test_files/colors.png"
 
 #Image Processing
-img = cv2.imread(fileName)
+img = cv2.imread(fileName) #(row, column) order
 unique_color_data = np.unique(img.reshape((1, -1, 3))[0,:],axis=0, return_counts=True)
-blacklist = np.array([[255,255,255], [0,0,0], [255,0,0]]) #NOTE, cv2 has pixels in bgr order
+blacklist = np.array([[255,255,255], [0,0,0]]) #NOTE, cv2 has pixels in bgr order
 
 for color in unique_color_data[0]:
     #Skips Colors On Blacklist
     if(np.any(np.all(blacklist == color, axis=-1))):
-        print(color)
         continue
 
     coords = np.where(np.all(img == color, axis=-1))
-    
-    for i in range(len(coords[0])):
-        img[coords[0][i],coords[1][i]] = [0,0,0]
+    coords = np.array((coords[0],coords[1])).T
+
+    area = len(coords)
+
+    #building triangle
+    points = []
+    line_segments = [(0,1),(0,2),(1,2)]
+
+    #rng trial
+    rng = np.random.randint(0,area,size=3)
+    for i in range(len(rng)):
+        points.append((coords[rng[i]][0],coords[rng[i]][1]))
+
+    # calculate scores
+    score = CalculateAccuracy(img, coords, points, line_segments)
+
+    for p in points:
+        img[p[0],p[1]] = [255,255,255]
 
 cv2.imshow("image", img)
 cv2.waitKey(0)
